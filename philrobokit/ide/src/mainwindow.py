a1b0e06f32122d0e1a0d27dcf1c077bec5beba99
julius.constante <julius.constante@philrobotics.com>
2011-11-02 13:44:59 +0000

    add initial File Menu item (add new file)

--- philrobokit/ide/src/mainwindow.py
+++ philrobokit/ide/src/mainwindow.py
@@ -19,9 +19,22 @@ class AppMainWindow(QtGui.QMainWindow):
         '''
         super(AppMainWindow, self).__init__()
         self.setWindowTitle("PhilRobokit IDE")
-        self.setMinimumSize(580, 600)
-        
+        self.setMinimumSize(300, 400)
+        self.resize(580, 600)
+                
         self.Editor = MultipleCppEditor(self)
         
         self.setCentralWidget(self.Editor)
+        
+        self.createActions()
+        self.createMenus()
+        
+    def createActions(self):
+        self.newAct = QtGui.QAction( "&New",
+                self, shortcut=QtGui.QKeySequence("Ctrl+N"),
+                statusTip="Create a new file", triggered=self.Editor.newFile)
+        
+    def createMenus(self):
+        self.fileMenu = self.menuBar().addMenu("&File")
+        self.fileMenu.addAction(self.newAct)
         
