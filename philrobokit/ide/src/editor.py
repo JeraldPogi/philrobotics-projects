8d4364fb668d83a57cf2cfcb37d4d9186c622be5
julius.constante <julius.constante@philrobotics.com>
2011-11-02 14:45:33 +0000

    implement File-Open and load contents to the editor

--- philrobokit/ide/src/editor.py
+++ philrobokit/ide/src/editor.py
@@ -5,14 +5,14 @@
 
 '''
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from PyQt4.Qsci import QsciScintilla, QsciLexerCPP
 
 class CppEditor(QsciScintilla):
     '''
     classdocs
     '''
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, fileName=None):
         '''
         Constructor
         '''
@@ -45,6 +45,25 @@ class CppEditor(QsciScintilla):
         
         # Enable folding visual- use boxes
         self.setFolding(QsciScintilla.BoxedTreeFoldStyle)
+        
+        
+        if fileName:
+            self.curFile = fileName
+            self.loadFile(fileName)
+            self.isUntitled = False
+        else:
+            self.curFile = "untitled.c"
+            self.isUntitled = True
+        
+    def loadFile(self, fileName):
+        qfile = QtCore.QFile(fileName)
+        if not qfile.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text):
+            QtGui.QMessageBox.warning(self, "CppEditor",
+                    "Cannot read file %s:\n%s." % (fileName, qfile.errorString()))
+            return False
+        instr = QtCore.QTextStream(qfile)
+        self.setText(instr.readAll())
+        return True
 
 
 class MultipleCppEditor(QtGui.QTabWidget):
@@ -63,8 +82,17 @@ class MultipleCppEditor(QtGui.QTabWidget):
     def newFile(self):
         child = CppEditor(self)
         self.addTab(child, "untitled.c")
+        self.setCurrentIndex(self.count()-1)        
+    def openFile(self):
+        fileName = QtGui.QFileDialog.getOpenFileName(
+                                        self, self.tr("Open Source File"),
+                                        "", "C source (*.c);;Text File (*.txt);;All files (*.*)" )
+        if fileName == "":
+            return False
+        child = CppEditor(self, fileName)
+        self.addTab(child, fileName.right(fileName.length() - fileName.lastIndexOf('/') - 1 ))
         self.setCurrentIndex(self.count()-1)
-        
+        return True    
     def closeFile(self):
         # todo: check if the file has change before closing
         self.removeTab(self.currentIndex())
