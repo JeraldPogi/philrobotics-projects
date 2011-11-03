44684341521dfe1e59c6576b3d8162afa116c38f
julius.constante <julius.constante@philrobotics.com>
2011-11-03 06:50:12 +0000

    add script for generating/building Win32 executables

---
+++ philrobokit/ide/src/setup_win32.py
@@ -0,0 +1,40 @@
+from cx_Freeze import setup, Executable
+
+includes = ['sip', 'PyQt4.QtCore']
+excludes = ['unicodedata', 'bz2' ]
+packages = []
+path = []
+files = []
+
+icons = ['new', 'open']
+for icon in icons:
+    file = 'images/' + icon + '.png'
+    files.append(file)
+
+exe = Executable(
+    script = 'main.pyw',
+    base = 'Win32GUI',
+    targetName = 'PhilRoboKitIDE.exe',
+    icon = None,
+    )
+ 
+setup(
+    name = "PhiRoboKit IDE",
+    description = 'PhiRoboKit Integrated development environment',
+    version = '0.1',
+    author = 'PhilRobotics',
+    options = {'build_exe': {
+                            'include_files' : files,
+                            'includes' : includes,
+                            'excludes' : excludes,
+                            'packages' : packages,
+                            'path' : path,
+                            'create_shared_zip' : False,
+                            'append_script_to_exe' : True,
+                            'include_in_shared_zip' : False,
+                        }
+              },
+    executables = [exe]
+    )
+
+
