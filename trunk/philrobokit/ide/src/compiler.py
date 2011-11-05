eec2e164caf5e84c99bcdb9f819d193687f241c9
julius.constante@philrobotics.com <julius.constante@philrobotics.com@ce0266c3-48f5-9131-0d75-cbc595ff65b2>
2011-11-05 07:08:18 +0000

    compiler check: trial project build  success! =) { *.hex output not yet verified }

--- trunk/philrobokit/ide/src/compiler.py
+++ trunk/philrobokit/ide/src/compiler.py
@@ -7,24 +7,41 @@
 
 import os
 import sys
+import subprocess
 
-#PhilRobokit Library Path
-PRK_LIB = 'libraries/PhilRobokitProjectLibrary'
+# library path
+LIB_DIR = 'libraries'
+# PhilRobokit Library
+PRK_LIB = LIB_DIR + '/PhilRobokitProjectLibrary'
+# output directory 
+OUT_DIR = 'philrobokit_output'
+
 
 class PicCompiler:
     '''
     classdocs
     '''
-    def __init__(self):
+    def __init__(self, chip='16F877A'):
         '''
         Constructor
         '''
+        
+        # PIC chip part
+        self.chip = chip
+        
+        # subprocess class    
+        self.proc = None
+        
+        self.isWin32Platform = False
+        
         if sys.platform == 'win32':
             self.toolpath = 'tools/picc_win32'
+            self.isWin32Platform = True
         elif sys.platform == 'linux2':
             self.toolpath = 'tools/picc_linux'
         else:
             self.toolpath = None
+            
         
     def getInfo(self):
         if self.toolpath:
@@ -40,7 +57,42 @@ class PicCompiler:
     def buildProject(self, userCode=None):
         if not os.path.isfile(userCode):
             return [False, 'file not found']
-        return [True, 'todo: start compiling ' + userCode]
+        #if not self.proc:
+        #    return [False, 'previous process ongoing']
+        
+        # output folder - same location with user code
+        outpath = os.path.dirname( str(userCode) ) + '/' + OUT_DIR
+        
+        # create output directory, if not existing
+        if not os.path.exists( outpath ):
+            os.makedirs( outpath )
+
+        PICC = os.getcwd() + '/' + self.toolpath + '/bin/picc'
+        
+        # todo: other compiler flags (e.g. hex output)
+        CMD = [PICC,
+               '--chip=' + self.chip,
+               '-I' + os.getcwd() + '/' + PRK_LIB,
+               '--OUTDIR=' + outpath,
+               userCode]
+        
+        commands = []
+        for cmd in CMD:
+            commands.append(str(cmd)) # Win32 workaround: QString problem
+        
+        self.shell = False
+        if self.isWin32Platform:
+            self.shell = True
+        
+        self.proc = subprocess.Popen( commands,
+                                      stdin=subprocess.PIPE,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE,
+                                      shell=self.shell )
+        
+        # todo: dont wait!; show progressbar
+        log = self.proc.stdout.read()
+        return [True, log]
         
         
         
