78515342e33711ae5202f8dd54bc0a085b1bc7c5
julius.constante@philrobotics.com <julius.constante@philrobotics.com@ce0266c3-48f5-9131-0d75-cbc595ff65b2>
2012-07-05 03:00:19 +0000

    better "Edit -> Find/Replace" (CTRL-F) dialog

---
+++ trunk/philrobokit/ide/src/cppeditor.py
@@ -0,0 +1,154 @@
+
+'''
+
+@filename: cppeditor.py
+
+'''
+
+from PyQt4 import QtGui, QtCore
+from PyQt4.Qsci import QsciScintilla, QsciLexerCPP
+
+# user source code
+PROJECT_ALIAS = 'PhilRobokit Proyekto' #'PhilRobokit Project'
+PROJECT_EXT = '.phr'
+PROJECT_NONAME = 'untitled'
+
+__default_content__ = '''
+void init()
+{
+
+}
+
+void program()
+{
+
+}
+'''
+
+class CppEditor(QsciScintilla):
+    '''
+    classdocs
+    '''
+    def __init__(self, parent=None, fileName=None):
+        '''
+        Constructor
+        '''
+        super(CppEditor, self).__init__(parent)
+        self.parent = parent
+        
+        self.setAcceptDrops(False) # drag&drop is on its parent
+        
+        # Set the default font
+        font = QtGui.QFont()
+        font.setFamily('Courier')
+        font.setFixedPitch(True)
+        font.setPointSize(10)
+        self.setFont(font)
+        self.setMarginsFont(font)
+        
+        # C/C++ lexer
+        self.lexer = QsciLexerCPP(self,  True)
+        self.lexer.setDefaultFont(font)
+        self.lexer.setAPIs(self.parent.getLibraryAPIs())
+        self.setLexer(self.lexer)
+        self.SendScintilla(QsciScintilla.SCI_STYLESETFONT, 1, 'Courier')
+        
+        # Auto-indent
+        self.setTabWidth(4)
+        #self.setIndentationsUseTabs(False)
+        self.setAutoIndent(True)
+        
+        # Current line visible with special background color
+        self.setCaretLineVisible(True)
+        self.setCaretLineBackgroundColor(QtGui.QColor("#ffe4e4"))
+        
+        # Enable brace matching
+        self.setBraceMatching(QsciScintilla.SloppyBraceMatch)
+        
+        # Enable folding visual- use boxes
+        self.setFolding(QsciScintilla.BoxedTreeFoldStyle)
+        
+        # show line numbers
+        fontmetrics = QtGui.QFontMetrics(font)
+        self.setMarginsFont(font)
+        self.setMarginWidth(0, fontmetrics.width("00000") + 4)
+        self.setMarginLineNumbers(0, True)
+        self.setMarginsBackgroundColor(QtGui.QColor("#eecc77"))
+        
+        # not too small
+        self.setMinimumSize(400, 200)
+        
+        # set the length of the string before the editor tries to auto-complete
+        self.setAutoCompletionThreshold(3)
+        # tell the editor we are using a QsciAPI for the auto-completion
+        self.setAutoCompletionSource(QsciScintilla.AcsAPIs)
+        # removed remaining right side characters from the current cursor
+        self.setAutoCompletionReplaceWord(True)
+        
+        # "CTRL+Space" autocomplete
+        self.shortcut_ctrl_space = QtGui.QShortcut(QtGui.QKeySequence("Ctrl+Space"), self)        
+        self.connect(self.shortcut_ctrl_space,
+            QtCore.SIGNAL('activated()'), self.autoCompleteFromAll)
+        
+        if fileName:
+            self.curFile = fileName
+            self.loadFile(fileName)
+            self.isUntitled = False
+        else:
+            self.curFile = PROJECT_NONAME + PROJECT_EXT
+            self.setText( __default_content__ )
+            self.isUntitled = True
+        
+        self.isModified = False
+        self.connect(self, QtCore.SIGNAL('textChanged()'), self.onTextChanged )    
+            
+    def onTextChanged(self):
+        self.isModified = True
+        self.parent.onChildContentChanged()
+        
+    def loadFile(self, fileName):
+        qfile = QtCore.QFile(fileName)
+        if not qfile.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text):
+            QtGui.QMessageBox.warning(self, PROJECT_ALIAS,
+                    "Cannot read file %s:\n%s." % (fileName, qfile.errorString()))
+            return False
+        instr = QtCore.QTextStream(qfile)
+        self.setText(instr.readAll())
+        return True
+    
+    def saveFile(self, fileName):
+        qfile = QtCore.QFile(fileName)
+        if not qfile.open(QtCore.QFile.WriteOnly | QtCore.QFile.Text):
+            QtGui.QMessageBox.warning(self, PROJECT_ALIAS,
+                    "Cannot write file %s:\n%s." % (fileName, qfile.errorString()))
+            return None
+
+        outstr = QtCore.QTextStream(qfile)
+        outstr << self.text()
+        
+        self.curFile = fileName
+        self.isUntitled = False
+        self.isModified = False
+        return fileName
+    
+    def saveAs(self):
+        fileName = QtGui.QFileDialog.getSaveFileName(self, "Save As",
+                self.curFile, PROJECT_ALIAS + " (*" + PROJECT_EXT + ");;" + 
+                    "C source (*.c);;Text File (*.txt);;All files (*.*)" )
+        if not fileName:
+            return None
+        return self.saveFile(fileName)
+    
+    def save(self):
+        if self.isUntitled:
+            return self.saveAs()
+        else:
+            return self.saveFile(self.curFile)
+        
+    def currentFile(self):
+        return self.curFile
+    
+    def modified(self):
+        return self.isModified
+    
+        
--- trunk/philrobokit/ide/src/editor.py
+++ trunk/philrobokit/ide/src/editor.py
@@ -6,172 +6,11 @@
 '''
 
 import os
-from PyQt4 import QtGui, QtCore, Qsci
-from PyQt4.Qsci import QsciScintilla, QsciLexerCPP
+from PyQt4 import QtGui, QtCore
+from PyQt4.Qsci import QsciAPIs, QsciLexerCPP
+from cppeditor import CppEditor, PROJECT_ALIAS, PROJECT_EXT, PROJECT_NONAME
+from finddialog import FindDialog
 from firmware import getLibraryKeywords
-
-# user source code
-PROJECT_ALIAS = 'PhilRobokit Proyekto' #'PhilRobokit Project'
-PROJECT_EXT = '.phr'
-PROJECT_NONAME = 'untitled'
-
-__default_content__ = '''
-void init()
-{
-
-}
-
-void program()
-{
-
-}
-'''
-
-class CppEditor(QsciScintilla):
-    '''
-    classdocs
-    '''
-    def __init__(self, parent=None, fileName=None):
-        '''
-        Constructor
-        '''
-        super(CppEditor, self).__init__(parent)
-        self.parent = parent
-        
-        self.setAcceptDrops(False) # drag&drop is on its parent
-        
-        # Set the default font
-        font = QtGui.QFont()
-        font.setFamily('Courier')
-        font.setFixedPitch(True)
-        font.setPointSize(10)
-        self.setFont(font)
-        self.setMarginsFont(font)
-        
-        # C/C++ lexer
-        self.lexer = QsciLexerCPP(self,  True)
-        self.lexer.setDefaultFont(font)
-        self.lexer.setAPIs(self.parent.getLibraryAPIs())
-        self.setLexer(self.lexer)
-        self.SendScintilla(QsciScintilla.SCI_STYLESETFONT, 1, 'Courier')
-        
-        # Auto-indent
-        self.setTabWidth(4)
-        #self.setIndentationsUseTabs(False)
-        self.setAutoIndent(True)
-        
-        # Current line visible with special background color
-        self.setCaretLineVisible(True)
-        self.setCaretLineBackgroundColor(QtGui.QColor("#ffe4e4"))
-        
-        # Enable brace matching
-        self.setBraceMatching(QsciScintilla.SloppyBraceMatch)
-        
-        # Enable folding visual- use boxes
-        self.setFolding(QsciScintilla.BoxedTreeFoldStyle)
-        
-        # show line numbers
-        fontmetrics = QtGui.QFontMetrics(font)
-        self.setMarginsFont(font)
-        self.setMarginWidth(0, fontmetrics.width("00000") + 4)
-        self.setMarginLineNumbers(0, True)
-        self.setMarginsBackgroundColor(QtGui.QColor("#eecc77"))
-        
-        # not too small
-        self.setMinimumSize(400, 200)
-        
-        # set the length of the string before the editor tries to auto-complete
-        self.setAutoCompletionThreshold(3)
-        # tell the editor we are using a QsciAPI for the auto-completion
-        self.setAutoCompletionSource(QsciScintilla.AcsAPIs)
-        
-        # "CTRL+Space" autocomplete
-        self.shortcut_ctrl_space = QtGui.QShortcut(QtGui.QKeySequence("Ctrl+Space"), self)        
-        self.connect(self.shortcut_ctrl_space,
-            QtCore.SIGNAL('activated()'), self.autoCompleteFromAll)
-        
-        self.stringToSearch = ""
-                
-        if fileName:
-            self.curFile = fileName
-            self.loadFile(fileName)
-            self.isUntitled = False
-        else:
-            self.curFile = PROJECT_NONAME + PROJECT_EXT
-            self.setText( __default_content__ )
-            self.isUntitled = True
-        
-        self.isModified = False
-        self.connect(self, QtCore.SIGNAL('textChanged()'), self.onTextChanged )    
-            
-    def onTextChanged(self):
-        self.isModified = True
-        self.parent.onChildContentChanged()
-        
-    def loadFile(self, fileName):
-        qfile = QtCore.QFile(fileName)
-        if not qfile.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text):
-            QtGui.QMessageBox.warning(self, PROJECT_ALIAS,
-                    "Cannot read file %s:\n%s." % (fileName, qfile.errorString()))
-            return False
-        instr = QtCore.QTextStream(qfile)
-        self.setText(instr.readAll())
-        return True
-    def saveFile(self, fileName):
-        qfile = QtCore.QFile(fileName)
-        if not qfile.open(QtCore.QFile.WriteOnly | QtCore.QFile.Text):
-            QtGui.QMessageBox.warning(self, PROJECT_ALIAS,
-                    "Cannot write file %s:\n%s." % (fileName, qfile.errorString()))
-            return None
-
-        outstr = QtCore.QTextStream(qfile)
-        outstr << self.text()
-        
-        self.curFile = fileName
-        self.isUntitled = False
-        self.isModified = False
-        return fileName
-    
-    def saveAs(self):
-        fileName = QtGui.QFileDialog.getSaveFileName(self, "Save As",
-                self.curFile, PROJECT_ALIAS + " (*" + PROJECT_EXT + ");;" + 
-                    "C source (*.c);;Text File (*.txt);;All files (*.*)" )
-        if not fileName:
-            return None
-        return self.saveFile(fileName)
-    
-    def save(self):
-        if self.isUntitled:
-            return self.saveAs()
-        else:
-            return self.saveFile(self.curFile)
-        
-    def currentFile(self):
-        return self.curFile
-    
-    def modified(self):
-        return self.isModified
-    
-    def findText(self, text=None):
-        if text:
-            self.stringToSearch = text
-        searchDlg = QtGui.QInputDialog(self)
-        repeate = True        
-        while repeate:            
-            searchResult = searchDlg.getText(self, "Find Text",
-                                             "type string to search:",
-                                             text=self.stringToSearch )
-            repeate = searchResult[1]
-            if repeate:
-                self.stringToSearch = searchResult[0]
-                self.findFirst(
-                            self.stringToSearch,
-                            False, # regular expression
-                            False, # case sensitive
-                            False, # whole word matches only
-                            True,  # wraps around
-                            True ) # forward
-        
 
 class MultipleCppEditor(QtGui.QTabWidget):
     '''
@@ -186,6 +25,8 @@ class MultipleCppEditor(QtGui.QTabWidget):
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         
         self.prepareLibraryAPIs()
+        
+        self.findDlg = FindDialog(self)
         
         self.setAcceptDrops(True)
         self.setTabShape(QtGui.QTabWidget.Triangular)
@@ -273,18 +114,80 @@ class MultipleCppEditor(QtGui.QTabWidget):
         child = self.currentWidget()
         return child.currentFile()
     
-    def findChildText(self, text=None):
+    def showFindDialog(self):
         child = self.currentWidget()
         if child:
-            return child.findText(text)
+            if child.hasSelectedText():
+                self.findDlg.setFindText(child.selectedText ())
+        self.findDlg.show()
+        
+    def findChildText(self, text=None, forwardDirection=True, caseSensitive=False,
+                      wrapSearch=True, wholeWord=False, regExp=False):
+        if text:
+            child = self.currentWidget()
+            if child:
+                #print "fw = ", forwardDirection, " cs = ", caseSensitive, " wrap = ", wrapSearch
+                result = child.findFirst( text,
+                            regExp, # regular expression
+                            caseSensitive, # case sensitive
+                            wholeWord, # whole word matches only
+                            wrapSearch,  # wraps around
+                            forwardDirection) # forward
+                return result
+        return False
     
+    def replaceChildText(self, newText=None):
+        child = self.currentWidget()
+        if child and child.hasSelectedText():
+            child.replaceSelectedText(newText) # returns None
+            return True                
+        return False
+    
+    def replaceFindChildText(self, oldText=None, newText=None, forwardDirection=True,
+                             caseSensitive=False, wrapSearch=True, wholeWord=False, regExp=False):
+        if oldText:
+            child = self.currentWidget()
+            if child:
+                child.replace(newText)
+                result = child.findFirst( oldText,
+                            regExp, # regular expression
+                            caseSensitive, # case sensitive
+                            wholeWord, # whole word matches only
+                            wrapSearch,  # wraps around
+                            forwardDirection) # forward
+                return result               
+        return False
+    
+    def replaceAllChildText(self, oldText=None, newText=None, caseSensitive=False,
+                            wholeWord=False, regExp=False):
+        if oldText:
+            if (not caseSensitive) and (oldText.lower() == newText.lower()):
+                print "nothing to replace"
+                return False
+            child = self.currentWidget()
+            if child:
+                result = child.findFirst( oldText,
+                            regExp, # regular expression
+                            caseSensitive, # case sensitive
+                            wholeWord, # whole word matches only
+                            True,  # wraps around
+                            True) # forward
+                if result:
+                    cnt = 0
+                    while cnt < 4096: # limit loop
+                        child.replace(newText)
+                        cnt += 1
+                        if not child.findNext(): break
+                    return True      
+        return False
+                
     def onChildContentChanged(self):
         title = self.tabText(self.currentIndex())
         if not title.contains('*', QtCore.Qt.CaseInsensitive):
             self.setTabText(self.currentIndex(), title + " * ")
         
     def prepareLibraryAPIs(self):
-        self.LibraryAPIs = Qsci.QsciAPIs(QsciLexerCPP(self,True))
+        self.LibraryAPIs = QsciAPIs(QsciLexerCPP(self,True))
         keywords = getLibraryKeywords()
         for keyword in keywords:
             self.LibraryAPIs.add( keyword )
