e9eb57d11be9f4283a2766b79cc5b449344a9229
julius.constante <julius.constante@philrobotics.com>
2012-07-20 11:35:33 +0000

    add copyright notice (GNU GPL)

--- philrobokit/ide/src/about.py
+++ philrobokit/ide/src/about.py
@@ -1,13 +1,43 @@
 
 '''
 
-@filename: about.py
+    @filename: about.py
+    @project : PhilRoboKit IDE
+
+    PhilRobotics | Philippine Electronics and Robotics Enthusiasts Club
+    http://philrobotics.com | http://philrobotics.com/forum | http://facebook.com/philrobotics
+    phirobotics.core@philrobotics.com
+
+    Copyright (C) 2012  Julius Constante
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses
 
 '''
 
 from PyQt4 import QtGui, QtCore
 
 SPLASH_IMAGE = 'images/about.png'
+
+SPLASH_NOTICE = '''
+  PhilRoboKit IDE  Copyright (C) 2012  PhilRobotics
+  This program comes with ABSOLUTELY NO WARRANTY.
+  This is free software, and you are welcome
+  to redistribute it under certain conditions.
+
+  
+
+'''
 
 class AboutDialog(QtGui.QSplashScreen):
     def __init__(self, parent=None):
@@ -17,8 +47,8 @@ class AboutDialog(QtGui.QSplashScreen):
         self.setPixmap(self.pix)
         self.setMask( self.pix.mask() )
         
-        self.showMessage("  loading modules . . .",
-                QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom, QtGui.QColor("#eecc77"));
+        self.showMessage( SPLASH_NOTICE + '    loading modules . . .',
+            QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom, QtGui.QColor("#eecc77"));
         
     def mousePressEvent(self, *args, **kwargs):
         # print 'you pressed me!'
--- philrobokit/ide/src/mainwindow.py
+++ philrobokit/ide/src/mainwindow.py
@@ -1,9 +1,27 @@
 
 '''
 
-@filename: mainwindow.py
-@author: 'yus
-@organization: PhilRobotics
+    @filename: mainwindow.py
+    @project : PhilRoboKit IDE
+
+    PhilRobotics | Philippine Electronics and Robotics Enthusiasts Club
+    http://philrobotics.com | http://philrobotics.com/forum | http://facebook.com/philrobotics
+    phirobotics.core@philrobotics.com
+
+    Copyright (C) 2012  Julius Constante
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses
 
 '''
 
@@ -31,7 +49,7 @@ class AppMainWindow(QtGui.QMainWindow):
         super(AppMainWindow, self).__init__()
         print "PhilRoboKit IDE started..."
         
-        if False: # todo: set to True is building stand-alone package (cx_Freeze)
+        if False: # todo: set to True if building stand-alone package (cx_Freeze)
             setpath = os.path.dirname( os.path.realpath( __file__ ) )
             if os.name == 'nt':
                 os.chdir( setpath[:setpath.rfind('\\')] )
