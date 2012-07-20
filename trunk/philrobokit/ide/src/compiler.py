7b7e6a85fcbc35a8834c278f1ddf51cdb4ba021d
julius.constante@philrobotics.com <julius.constante@philrobotics.com@ce0266c3-48f5-9131-0d75-cbc595ff65b2>
2012-07-20 11:35:33 +0000

    add copyright notice (GNU GPL)

--- trunk/philrobokit/ide/src/compiler.py
+++ trunk/philrobokit/ide/src/compiler.py
@@ -1,7 +1,27 @@
 
 '''
 
-@filename: compiler.py
+    @filename: compiler.py
+    @project : PhilRoboKit IDE
+
+    PhilRobotics | Philippine Electronics and Robotics Enthusiasts Club
+    http://philrobotics.com | http://philrobotics.com/forum | http://facebook.com/philrobotics
+    phirobotics.core@philrobotics.com
+
+    Copyright (C) 2012  Julius Constante
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses
 
 '''
 
