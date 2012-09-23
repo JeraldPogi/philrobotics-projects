04f1a60b79ccfbf9966b468e3ec8bd7ddfdbefe7
keith.beja@philrobotics.com <keith.beja@philrobotics.com@ce0266c3-48f5-9131-0d75-cbc595ff65b2>
2012-09-23 15:53:05 +0000

    Modified I2C library

--- trunk/philrobokit/firmware/UserLib/i2c/i2c.c
+++ trunk/philrobokit/firmware/UserLib/i2c/i2c.c
@@ -1,13 +1,14 @@
 //***********************************************************************************
-// PhilRobotics | Amateur Robotics Club of the Philippines
-// http://philrobotics.com | http://facebook.com/philrobotics
+// PhilRobotics | Philippine Electronics and Robotics Enthusiasts Club
+// http://philrobotics.com | http://philrobotics.com/forum | http://facebook.com/philrobotics
 // phirobotics.core@philrobotics.com
 //
 //----------------------------------------------------------------------------------
 // Filename:	i2c.c - I2C File
 // Description:	
-// Revision:    v00.01.00
+// Revision:    v00.00.02
 // Author:      Giancarlo Acelajado
+//				Keith Beja
 //
 // Dependencies:
 //
@@ -24,140 +25,153 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //***********************************************************************************
 // FW Version      Date        Author         Description
-// v00.00.01       201202xx    Giancarlo A.   Library Initial Release
-// 
+// v00.00.01       201202xx    Giancarlo A.   - Library Initial Release
+// v00.00.02	   20120829	   Keith B.		  - Code Cleanup
+//											  - Modified I2C pins to be tri-state instead of output high
+//											  - Added i2c setup feature
 //***********************************************************************************
 
 //Based from Regulus Berdin I2C routines for PIC Microcontroller
 //http://www.electronicslab.ph/forum/index.php?topic=4679.msg76635#msg76635
-
 #include "i2c.h"
 
-	//--- Private Functions ------------------------------	
-	void i2cSendAck(unsigned char ucSDAPin, unsigned char ucSCLPin, char blAcknowledge)
-	{
-		setPinLow(ucSCLPin); //Set SCL Pin to Low
-		//Optional Delay Here
-	
-		if(blAcknowledge){
-			setPinLow(ucSDAPin); //Set SDA Pin to Low
-		}
-		else{
-			setPinHigh(ucSDAPin); //Set SDA Pin to High
-		}
+/* Local Variables */
+enum i2cInfo
+{
+	I2C_SDAPIN
+	,I2C_SCLPIN
+	,MAXI2CINFO
+};
 
-		//Optional Delay Here
-		setPinHigh(ucSCLPin); //Set SCL Pin to High
-		//Optional Delay Here
-		setPinLow(ucSCLPin); //Set SCL Pin to Low
-		//Optional Delay Here
-	
-		if(blAcknowledge){
-			setPinHigh(ucSDAPin); //Set SDA Pin to High
-		}
-	}
-	
-	//--- End of Private Functions ------------------------------
-	void i2cStart(unsigned char ucSDAPin, unsigned char ucSCLPin)
-	{
-		setPinHigh(ucSCLPin); //Set SCL Pin to High
-		//Optional Delay Here
-		setPinHigh(ucSDAPin); //Set SDA Pin to High
-		//Optional Delay Here
-		setPinLow(ucSDAPin); //Set SDA Pin to Low		
-		//Optional Delay Here
-		setPinLow(ucSCLPin); //Set SCL Pin to Low
-		//Optional Delay Here
-	}
-	
-	void i2cStop(unsigned char ucSDAPin, unsigned char ucSCLPin)
-	{
-		setPinLow(ucSDAPin); //Set SDA Pin to Low		
-		//Optional Delay Here
-		setPinHigh(ucSCLPin); //Set SCL Pin to High
-		//Optional Delay Here
-		setPinHigh(ucSDAPin); //Set SDA Pin to High
-		//Optional Delay Here
-	}
-	
-	void setupI2C(unsigned char ucSDAPin, unsigned char ucSCLPin)
-	{
-		setPinOutput(ucSDAPin); //Config SDA Pin as Output
-		setPinOutput(ucSCLPin); //Config SCL Pin as Output
-		
-		setPinHigh(ucSCLPin); //Set SCL Pin to High
-		setPinHigh(ucSDAPin); //Set SDA Pin to High
-	}
-	
-	void i2cWrite(unsigned char ucSDAPin, unsigned char ucSCLPin, unsigned char i2cData)
-	{
-		int i = 8;		
-		
-		setPinLow(ucSCLPin); //Set SCL Pin to Low    
-		setPinOutput(ucSDAPin); //Config SDA Pin as Output
-    
-		do {
-			if (i2cData & 0x80) {
-				setPinHigh(ucSDAPin); //Set SDA Pin to High
-			} 
-			else {
-				setPinLow(ucSDAPin); //Set SDA Pin to Low
-			}
-        
-			//Optional Delay Here
-			setPinHigh(ucSCLPin); //Set SCL Pin to High
-			//Optional Delay Here
-			setPinLow(ucSCLPin); //Set SCL Pin to Low
-			//Optional Delay Here
-			i2cData<<=1; //Shift Data
-		} while (--i);
+static uint8_t i2c[MAXI2CMOD][MAXI2CINFO] = {0};
 
-		//get acknowledge
-		setPinHigh(ucSDAPin); //Set SDA Pin to High
-		setPinHigh(ucSCLPin); //Set SCL Pin to High
-		//Optional Delay here
+static uint8_t ui8Ctr;
+static uint8_t ui8i2cData;
+
+/* Private Function Prototypes */
+static void i2cSendAck(enum i2cModules i2cMod, bool blAcknowledge);	
+
+/* Public Functions */
+void i2cSetup(enum i2cModules i2cMod, uchar_t ucSDAPin, uchar_t ucSCLPin)
+{
+	i2c[i2cMod][I2C_SDAPIN] = ucSDAPin;
+	i2c[i2cMod][I2C_SCLPIN] = ucSCLPin;
+}
+
+void i2cStart(enum i2cModules i2cMod)
+{
+	setSCL(); //Set SCL Pin to High
+	//Optional Delay Here
+	setSDA(); //Set SDA Pin to High
+	//Optional Delay Here
+	clrSDA(); //Set SDA Pin to Low		
+	//Optional Delay Here
+	clrSCL(); //Set SCL Pin to Low
+	//Optional Delay Here
+}
+
+void i2cStop(enum i2cModules i2cMod)
+{
+	clrSDA(); //Set SDA Pin to Low		
+	//Optional Delay Here
+	setSCL(); //Set SCL Pin to High
+	//Optional Delay Here
+	setSDA(); //Set SDA Pin to High
+	//Optional Delay Here
+}
+
+uchar_t i2cWrite(enum i2cModules i2cMod, uchar_t i2cData)
+{
+	ui8Ctr = 8;	
+	
+	clrSCL(); //Set SCL Pin to Low
+
+	do {
+		if (i2cData & 0x80) {
+			setSDA(); //Set SDA Pin to High
+		} 
+		else {
+			clrSDA(); //Set SDA Pin to Low
+		}
+	
+		//Optional Delay Here
+		setSCL(); //Set SCL Pin to High
+		//Optional Delay Here
+		clrSCL(); //Set SCL Pin to Low
+		//Optional Delay Here
+		i2cData<<=1; //Shift Data
+	} while (--ui8Ctr);
+
+	//get acknowledge
+	setSDA(); //Set SDA Pin to High
+	setSCL(); //Set SCL Pin to High
+	//Optional Delay here
+	
+	if (isPinHigh(i2c[i2cMod][I2C_SDAPIN])) {
+		//SDA remains high, not acknowledged
+		//Optional Delay Here
+		clrSCL(); //Set SCL Pin to Low
+		//Optional Delay Here
+		return I2C_ERROR_NO_ACK;
+	}
+	
+	//Optional Delay Here
+	clrSCL(); //Set SCL Pin to Low
+	//Optional Delay Here
+	
+	return I2C_NO_ERROR;	
+}
+
+uchar_t i2cRead(enum i2cModules i2cMod, bool blAcknowledge)
+{
+	ui8Ctr = 8;
+	ui8i2cData = 0;
+	
+	clrSCL(); //Set SCL Pin to Low
+	//Optional Delay Here
+	setSDA(); //Set SDA Pin to High
+	
+	do {
+		ui8i2cData<<=1;
+		setSCL(); //Set SCL Pin to High
+		//Optional Delay Here
 		
-		if (isPinHigh(ucSDAPin)) {
-			//SDA remains high, not acknowledged
-			//Optional Delay Here
-			setPinLow(ucSCLPin); //Set SCL Pin to Low
-			//Optional Delay Here
-			return I2C_ERROR_NO_ACK;
+		if (isPinHigh(i2c[i2cMod][I2C_SDAPIN])) {
+			ui8i2cData |= 1;
 		}
 		
 		//Optional Delay Here
-		setPinLow(ucSCLPin); //Set SCL Pin to Low
+		clrSCL(); //Set SCL Pin to Low
 		//Optional Delay Here
-		
-		return I2C_NO_ERROR;	
+	} while (--ui8Ctr);
+	
+	i2cSendAck(i2cMod, blAcknowledge); //Send Acknowledge Routine
+	
+	return ui8i2cData;
+}
+
+/* Private Functions */
+static void i2cSendAck(enum i2cModules i2cMod, bool blAcknowledge)
+{
+	clrSCL(); //Set SCL Pin to Low
+	//Optional Delay Here
+
+	if(blAcknowledge){
+		clrSDA(); //Set SDA Pin to Low
 	}
-	
-	unsigned char i2cRead(unsigned char ucSDAPin, unsigned char ucSCLPin, char blAcknowledge)
-	{
-		int i = 8;	
-		unsigned char i2cData = 0;
-		
-		setPinLow(ucSCLPin); //Set SCL Pin to Low
-		//Optional Delay Here
-		setPinHigh(ucSDAPin); //Set SDA Pin to High
-		
-		do {
-			i2cData<<=1;
-			setPinHigh(ucSCLPin); //Set SCL Pin to High
-			//Optional Delay Here
-			
-			if (isPinHigh(ucSDAPin)) {
-				i2cData |= 1;
-			}
-			
-			//Optional Delay Here
-			setPinLow(ucSCLPin); //Set SCL Pin to Low
-			//Optional Delay Here
-		} while (--i);
-		
-		i2cSendAck(ucSDAPin, ucSCLPin, blAcknowledge); //Send Acknowledge Routine
-		
-		return i2cData;
+	else{
+		setSDA(); //Set SDA Pin to High
 	}
-	
-	
+
+	//Optional Delay Here
+	setSCL(); //Set SCL Pin to High
+	//Optional Delay Here
+	clrSCL(); //Set SCL Pin to Low
+	//Optional Delay Here
+
+	if(blAcknowledge){
+		setSDA(); //Set SDA Pin to High
+	}
+}
+/* end of i2c.c */
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------	
