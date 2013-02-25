fe0f7abafe695df9cfdf72ea6810a5abd3cb3170
efren.cruzat@philrobotics.com <efren.cruzat@philrobotics.com@ce0266c3-48f5-9131-0d75-cbc595ff65b2>
2013-02-25 03:53:37 +0000

    - added draft of soft dac library

---
+++ trunk/philrobokit/firmware/Development/UserLib/soft_dac/soft_dac.h
@@ -0,0 +1,66 @@
+/*****************************************************************************************//**
+* # PhilRobotics | Philippine Electronics and Robotics Enthusiasts Club #
+* http://philrobotics.com | http://philrobotics.com/forum | http://facebook.com/philrobotics
+* phirobotics.core@philrobotics.com
+*
+*---------------------------------------------------------------------------------------------
+* |Filename:      | "soft_dac.h"                                |
+* |:----          |:----                                        |
+* |Description:   | This is a header file for the Software Implemented DAC library   |
+* |Revision:      | v00.00.01                                   |
+* |Author:        | Efren S. Cruzat II                          |
+* |               |                                             |
+* |Dependencies:  |                                             |
+*
+* > This program is free software: you can redistribute it and/or modify
+* > it under the terms of the GNU General Public License as published by
+* > the Free Software Foundation, either version 3 of the License, or
+* > (at your option) any later version.
+* > This program is distributed in the hope that it will be useful,
+* > but WITHOUT ANY WARRANTY; without even the implied warranty of
+* > MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* > GNU General Public License for more details.
+* > <BR><BR>
+* > You should have received a copy of the GNU General Public License
+* > along with this program. If not, see http://www.gnu.org/licenses/
+* <BR>
+*---------------------------------------------------------------------------------------------
+* |FW Version   |Date       |Author             |Description                |
+* |:----        |:----      |:----              |:----                      |
+* |v00.00.01    |20130221   |ESCII              |Library Initial Release    |
+*********************************************************************************************/
+#define __SHOW_MODULE_HEADER__ /*!< \brief This section includes the Module Header on the documentation */
+#undef  __SHOW_MODULE_HEADER__
+
+#ifndef __PH_SOFTDAC_H__
+#define __PH_SOFTDAC_H__
+
+/* Include .h Library Files */
+#include <PhilRoboKit_CoreLib_Macro.h>
+#include "corelib_8bit_timer.h"
+
+/* User Configuration Definitions */
+    /* none */
+    
+/* Global Constants */
+enum eSoftDAC_Module
+{
+    SDAC0
+    ,SDAC1
+    ,SDAC2
+    ,SDAC3
+    ,MAX_NUM_OF_SDAC
+};
+
+#define K_SOFT_DAC_PERIOD     255       // 390.625Hz (2.56mS)
+
+/* Macro and Configuration Definitions */
+    /* none */
+    
+/* Public Function Prototypes */
+void setupSoftDAC(uint8_t ui8SDACModule, uint8_t ui8Pin, uint8_t ui8Value);
+void setSoftDAC(uint8_t ui8SDACModule, uint8_t ui8Value) ;
+void removeSoftDAC(uint8_t ui8SDACModule);
+
+#endif /* end of soft_dac.h */
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------	
