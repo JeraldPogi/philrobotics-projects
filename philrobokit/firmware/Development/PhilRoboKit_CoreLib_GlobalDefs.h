41490e956f6e24df2396ab9bfe00b28faba5bb7b
efren.cruzat <efren.cruzat@philrobotics.com>
2013-04-13 00:25:45 +0000

    - added global defs and config headers

---
+++ philrobokit/firmware/Development/PhilRoboKit_CoreLib_GlobalDefs.h
@@ -0,0 +1,56 @@
+/*****************************************************************************************//**
+* # PhilRobotics | Philippine Electronics and Robotics Enthusiasts Club #
+* http://philrobotics.com | http://philrobotics.com/forum | http://facebook.com/philrobotics
+* phirobotics.core@philrobotics.com
+*
+*---------------------------------------------------------------------------------------------
+* |Filename:      | "PhilRoboKit_CoreLib_GlobalDefs.h"          |
+* |:----          |:----                                        |
+* |Description:   | Global Variable Definitions                 |
+* |Revision:      | v00.00.01                                   |
+* |Author:        | Efren S. Cruzat II                          |
+* |               |                                             |
+* |Dependencies:  |                                             |
+*
+* > This program is free software: you can redistribute it and/or modify
+* > it under the terms of the GNU General Public License as published by
+* > the Free Software Foundation, either version 3 of the License, or
+* > (at your option) any later version.
+* > This program is distributed in the hope that it will be useful,
+* > but WITHOUT ANY WARRANTY; without even the implied warranty of
+* > MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* > GNU General Public License for more details.
+* > <BR><BR>
+* > You should have received a copy of the GNU General Public License
+* > along with this program. If not, see http://www.gnu.org/licenses/
+* <BR>
+*---------------------------------------------------------------------------------------------
+* |FW Version   |Date       |Author             |Description                        |
+* |:----        |:----      |:----              |:----                              |
+* |v00.00.01    |20120713   |ESCII              |Library Initial Release            |
+*********************************************************************************************/
+#define __SHOW_MODULE_HEADER__ /*!< \brief This section includes the Module Header on the documentation */
+#undef  __SHOW_MODULE_HEADER__
+
+#ifndef __ANITO_GLOBALS_C__
+#define __ANITO_GLOBALS_C__
+
+/* Include .h Library Files */
+#include "PhilRoboKit_CoreLib_DataTypes.h"
+#include "corelib_test.h"
+
+/* User Configuration Definitions */
+    /* none */
+
+/* Global Constants */ 
+    /* none */
+
+/* Macro and Configuration Definitions */  
+    /* none */
+
+/* Public Function Prototypes */ 
+void set_gblInitialized_FlagValue(void);
+bool_t get_gblInitialized_FlagValue(void);
+
+#endif/* end of PhilRoboKit_CoreLib_GlobalDefs.c */
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
